7f35e9510456afb3801bdf88a909609b8ba6bc4d
DTH235673-NguyenHoangKhang <130205khang@gmail.com>
2026-03-19 19:16:32 +0700

    Update

--- routers/dinhdang.js
+++ routers/dinhdang.js
@@ -3,26 +3,50 @@ var router = express.Router();
 var DinhDang = require('../models/dinhdang');
 // GET: Danh sách 
 router.get('/', async (req, res) => {
- 
+    var dd=await DinhDang.find();
+    res.render('dinhdang', {
+        title: 'Danh sách định dạng',
+        dinhdang: dd
+    });
 });
 // GET: Thêm 
 router.get('/them', async (req, res) => {
- 
+     res.render('dinhdang_them', {
+        title: 'Thêm định dạng'
+    });
 });
 // POST: Thêm 
 router.post('/them', async (req, res) => {
- 
+    var data=
+        {
+            TenDinhDang: req.body.TenDinhDang
+        }
+        await DinhDang.create(data);
+        res.redirect('/dinhdang');
 });
 // GET: Sửa
 router.get('/sua/:id', async (req, res) => {
- 
+    var id=req.params.id;
+    var dd=await DinhDang.findById(id);
+    res.render('dinhdang_sua', {
+        title: 'Sửa định dạng',
+        dinhdang: dd
+    });
 });
 // POST: Sửa 
 router.post('/sua/:id', async (req, res) => {
- 
+    var id=req.params.id;
+        var data=
+        {
+            TenDinhDang: req.body.TenDinhDang
+        }
+    await DinhDang.findByIdAndUpdate(id,data);
+    res.redirect('/dinhdang');
 });
 // GET: Xóa 
 router.get('/xoa/:id', async (req, res) => {
- 
+    var id=req.params.id;
+    await DinhDang.findByIdAndDelete(id);
+    res.redirect('/dinhdang');
 });
 module.exports = router;
