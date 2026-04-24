eb0776c04c2d44f23c862871d83ed25b2c27173a
DTH235673-Khang <130205khang@gmail.com>
2026-04-24 09:08:59 +0700

    Update

--- models/suatchieu.js
+++ models/suatchieu.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var suatChieuSchema = new mongoose.Schema({
- start: { type: String, unique: true, required: true },
- end: { type: String, unique: true, required: true },
+ start: { type: String, required: true },
+ end: { type: String, required: true },
  Phim: { type: mongoose.Schema.Types.ObjectId, ref: 'Phim' },
  PhongChieu: { type: mongoose.Schema.Types.ObjectId, ref: 'PhongChieu' },
  NgayChieu: { type: Date, required: true },
